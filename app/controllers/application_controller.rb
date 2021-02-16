41a0a4352aba529f3abe4ce1b08f73c2e76e5a52
teskew <teskinder1@gmail.com>
2021-02-16 13:45:33 -0600

    commit tree 26d3fa35f439b33b1eb79831f2256b3acc685462

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,56 @@ require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+     configure do
+       set :public_folder, 'public'
+       set :views, 'app/views'
+     end
 
-  get '/' do
-  end
-end
+     get '/' do	
+      
+    end
+
+
+    get '/' do	
+      redirect to '/articles/:id'
+    end
+
+    get '/articles' do
+      @articles = Article.all
+      erb :index
+    end
+
+    get '/articles/new' do
+      @article = Article.new
+      erb :new
+    end
+
+    post '/articles' do
+      @article = Article.create(title: params[:title], content: params[:content])
+      redirect to "/articles/#{@article.id}"
+    end
+
+    get '/articles/:id' do
+      @article = Article.find(params[:id])
+      erb :show
+    end
+
+    get '/articles/:id/edit' do
+      @article = Article.find(params[:id])
+      erb :edit
+    end
+
+
+    patch "/articles/:id" do
+      @article = Article.find(params[:id])
+      # @article.update(title: params[:title], content: params[:content])
+      @article.update(params[:article])
+      redirect to "/articles/#{ @article.id }"
+    end
+
+
+    delete '/articles/:id' do
+      Article.delete(params[:id])
+      redirect to "/articles"
+    end
+  end	 
